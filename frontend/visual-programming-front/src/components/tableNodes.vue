6dc97506eebec6da829a3a047d15d1d30a79aac7
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2022-09-08 15:45:33 -0500

    Update tableNodes.vue

--- frontend/visual-programming-front/src/components/tableNodes.vue
+++ frontend/visual-programming-front/src/components/tableNodes.vue
@@ -1,64 +1,137 @@
 <template>
   <el-container class="container">
     <el-main>
-    <EasyDataTable
-    theme-color="#1d90ff"
-      v-model:items-selected="itemsSelected"
-      show-index
-      :headers="headers"
-      :items="items"
-      @click-row="showRow"
-    />
-  </el-main>
+      <EasyDataTable
+        theme-color="#1d90ff"
+        table-class-name="customize-table"
+        header-text-direction="center"
+        body-text-direction="center"
+        v-model:items-selected="itemsSelected"
+        show-index
+        :headers="headers"
+        :items="items"
+        @click-row="showRow"
+      />
+    </el-main>
   </el-container>
+  <!-- Dialog -->
   <el-dialog
     v-model="outerVisible"
     title="Export"
     width="70%"
   >
-  <span>Code:</span>
-  <pre><code></code></pre>
-  <span>Console:</span>
-  <pre><code></code></pre>
+    <template #header>
+      <div class="my-header">
+        <el-button type="success" @click="runProgram">
+          <el-icon :size="30" class="el-icon--left"><CaretRight /></el-icon>
+            Run
+        </el-button>
+      </div>
+    <!-- Code & Console-->
+    </template>
+    <span>Code:</span>
+    <pre><code>{{programSelect}}</code></pre>
+    <span>Console:</span>
+    <pre><code>{{consoleData}}</code></pre>
     <template #footer>
       <span class="dialog-footer">
-        
-        >
       </span>
     </template>
   </el-dialog>
-  row clicked:<br />
-  <div id="row-clicked"></div>
 </template>
-  
-  <script lang="ts" setup>
+
+
+<script lang="ts">
   import { ref } from 'vue'
   import type { Header, Item, ClickRowArgument } from "vue3-easy-data-table";
-
-  const outerVisible = ref(false)
-  const showRow = (item: ClickRowArgument) => {
-    outerVisible.value = true
-    console.log((item))
-    document.getElementById('row-clicked').innerHTML = JSON.stringify(item);
-};
+  import EventService from "@/services/endpoints.js";
 
 
-  
-const headers: Header[] = [
-  { text: "PLAYER", value: "player" },
-  { text: "TEAM", value: "team"},
-];
+  export default {
+    setup() {
+      const consoleData = ref("")
+      const programSelect = ref("")
+      const dataItems = ref([])
+      const outerVisible = ref(false)
+      const showRow = (item: ClickRowArgument) => {
+        programSelect.value = item.body
+        consoleData.value = ""
+        outerVisible.value = true
+        console.log((item))
+      };
+      const headers: Header[] = [
+        { text: "USERNAME", value: "name" },
+        { text: "PROGRAM NAME", value: "program_name"},
+      ];
+      const items: Item[] = dataItems
 
-const items: Item[] = [
-  { player: "Stephen Curry", team: "GSW", number: 30, position: 'G', indicator: {"height": '6-2', "weight": 185}, lastAttended: "Davidson", country: "USA"},
-  { player: "Lebron James", team: "LAL", number: 6, position: 'F', indicator: {"height": '6-9', "weight": 250}, lastAttended: "St. Vincent-St. Mary HS (OH)", country: "USA"},
-  { player: "Kevin Durant", team: "BKN", number: 7, position: 'F', indicator: {"height": '6-10', "weight": 240}, lastAttended: "Texas-Austin", country: "USA"},
-  { player: "Giannis Antetokounmpo", team: "MIL", number: 34, position: 'F', indicator: {"height": '6-11', "weight": 242}, lastAttended: "Filathlitikos", country: "Greece"},
-];
+      programs()
+      function programs() {
+        (async () => {
+          const res = await EventService.getPrograms();
+          dataItems.value = res.data.foo
+          console.log(dataItems)
+        })()
+      }
 
-  </script>
-  <style>
-    .container {
+      function runProgram() {
+        (async () => {
+          const res = await EventService.runProgram({"code": programSelect.value});
+          consoleData.value = res.data.output
+        })()
+      }
+
+      return {
+        showRow, headers, items, dataItems, outerVisible, programSelect, runProgram, consoleData
+      }
+    } 
+  }
+</script>
+
+
+<style>
+  .container {
     min-height: calc(100vh - 100px);
-    }
-  </style>
+  }
+
+  .customize-table {
+    --easy-table-border: 1px solid #445269;
+    --easy-table-row-border: 1px solid #445269;
+
+    --easy-table-header-font-size: 14px;
+    --easy-table-header-height: 50px;
+    --easy-table-header-font-color: #c1cad4;
+    --easy-table-header-background-color: #2d3a4f;
+
+    --easy-table-header-item-padding: 10px 15px;
+
+    --easy-table-body-even-row-font-color: #fff;
+    --easy-table-body-even-row-background-color: #273958;
+
+    --easy-table-body-row-font-color: #c0c7d2;
+    --easy-table-body-row-background-color: #21577b;
+    --easy-table-body-row-height: 50px;
+    --easy-table-body-row-font-size: 14px;
+
+    --easy-table-body-row-hover-font-color: #2d3a4f;
+    --easy-table-body-row-hover-background-color: #eee;
+
+    --easy-table-body-item-padding: 10px 15px;
+
+    --easy-table-footer-background-color: #2d3a4f;
+    --easy-table-footer-font-color: #c0c7d2;
+    --easy-table-footer-font-size: 14px;
+    --easy-table-footer-padding: 0px 10px;
+    --easy-table-footer-height: 50px;
+
+    --easy-table-rows-per-page-selector-width: 70px;
+    --easy-table-rows-per-page-selector-option-padding: 10px;
+
+    --easy-table-scrollbar-track-color: #2d3a4f;
+    --easy-table-scrollbar-color: #2d3a4f;
+    --easy-table-scrollbar-thumb-color: #4c5d7a;;
+    --easy-table-scrollbar-corner-color: #2d3a4f;
+
+    --easy-table-loading-mask-background-color: #2d3a4f;
+  }
+</style>
